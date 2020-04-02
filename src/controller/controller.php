445cb7edeb7bbc0144152e4f93377e6b09c3c07d
Adrien GATINEAU <adgat34@gmail.com>
2020-04-02 11:37:40 +0200

    User CRUD + Validation form + session + administration post

--- src/controller/controller.php
+++ src/controller/controller.php
@@ -4,17 +4,31 @@ namespace App\src\controller;
 
 use App\src\DAO\PostDAO;
 use App\src\DAO\CommentDAO;
+use App\config\Request;
+use App\src\Constraint\Validation;
 
 abstract class Controller extends TwigController
 {
     protected $twig;
     protected $commentDAO;
     protected $postDAO;
+    private $request;
+    protected $get;
+    protected $post;
+    protected $session;
+    protected $validation;
     
     public function __construct()
     {
         $this->twig = parent::twigLoader();
-        $this->postDAO = new postDAO();
+        $this->postDAO = new PostDAO();
         $this->commentDAO = new CommentDAO();
+        
+        $this->validation = new Validation();
+        $this->request = new Request();
+        
+        $this->get =  $this->request->getGet();
+        $this->post = $this->request->getPost();
+        $this->session = $this->request->getSession();
     }
 }
